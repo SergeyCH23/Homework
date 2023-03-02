ccb4b401c0a18d58a59e86930fadf237bc6888b9
Sergey <chhet2015@gmail.com>
2023-03-02 12:35:59 +0300

    Homework

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿
+// ДОМАШНЯЯ РАБОТА
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("Введите первое число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+//  Console.Write("Введите второе число: ");
+//  int a = Convert.ToInt32(Console.ReadLine());
+
+// if ( a>b) // Точка с запятой не ставится перед "{"
+// {
+//     Console.WriteLine("max = "  + a );
+// }
+// else
+// {
+//     Console.WriteLine("max = " + b);
+// }   
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+ 
+//  Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+//   Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+ 
+//  if (a>b)
+//  {
+//     if (a>c)
+//     {
+//         Console.WriteLine("max = " + a);
+//     }
+//     else 
+//     {
+//         Console.WriteLine("max = " + c);
+//     }
+//  }
+//  else 
+//  {
+//     if (b>c)
+//     {
+//         Console.WriteLine("max = " + b);
+//     }
+//     else 
+//     {
+//         Console.WriteLine("max = " + c);
+//     }
+//  }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.Write("Введите число: ");
+// int value = Convert.ToInt32(Console.ReadLine());
+
+// if (value %2 == 0)
+// {
+//     Console.WriteLine(value + " да четное");
+// }
+// else 
+// {
+//     Console.WriteLine(value + " нет нечетное");
+// }
